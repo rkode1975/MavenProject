5e1ce529ea9a552085735deac583643cd0c0fa0d
thulsamrala <thulsamrala.samy.hcl@gmail.com>
2017-01-31 18:53:25 -0700

    Update App.java
    changed from git server

--- src/main/java/com/learntek/devops/myproject/App.java
+++ src/main/java/com/learntek/devops/myproject/App.java
@@ -12,5 +12,7 @@ public class App
 System.out.println( "Hello DevOpsWorld!" );
 System.out.println( "Hello DevOpsWorld Diffrence!" );
 System.out.println( "Hello DevOpsWorld Develop branch!" );
-System.out.println( "Hello DevOpsWorld sprint1 branch intermediate changes..! -- From Source TRee--" );    }
+System.out.println( "Hello DevOpsWorld sprint1 branch intermediate changes..! -- From Source TRee--" ); 
+System.out.println( "Git hub remote change" );
+    }
 }
